98518e5e6bd306c8938b84654cfd63182df58dc9
Humen4ik <dmitrog61@gmail.com>
2023-12-11 12:30:09 +0200

    added some changes to a method

--- DZ_7_Lists/src/Main.java
+++ DZ_7_Lists/src/Main.java
@@ -27,11 +27,12 @@ public class Main {
         Record record1 = new Record("Dmytro", "098 111 11 11");
         Record record2 = new Record("Dmytro", "098 222 22 22");
         Record record3 = new Record("Jake", "098 333 33 33");
-        Record record4 = new Record("Adolf", "098 444 44 44");
+        Record record4 = new Record("Robert", "098 444 44 44");
         phoneDirectory.add(record1);
         phoneDirectory.add(record2);
         phoneDirectory.add(record3);
         phoneDirectory.add(record4);
+        System.out.println(phoneDirectory.find("Robert"));
         System.out.println(phoneDirectory.findAll("Dmytro"));
     }
 
@@ -84,15 +85,30 @@ public class Main {
         for (String s : list) {
             wordsNumber.put(s, wordsNumber.getOrDefault(s, 0) + 1);
         }
-        System.out.println(wordsNumber);
+        for (Map.Entry<String, Integer> entry : wordsNumber.entrySet()) {
+            System.out.printf("%s: %d,%n", entry.getKey(), entry.getValue());
+        }
     }
-    public static void findOccurence(List<String> list) {
-        Map<String, Integer> wordsNumber = new HashMap<>();
-        for (String s : list)
-            wordsNumber.put(s, wordsNumber.getOrDefault(s, 0) + 1);
+    public static List<WordOccurence> findOccurence(List<String> list) {
+//        Map<String, Integer> wordsNumber = new HashMap<>();
+//        for (String s : list)
+//            wordsNumber.put(s, wordsNumber.getOrDefault(s, 0) + 1);
+//
+//        Set<Map.Entry<String, Integer>> listWordsNumber = wordsNumber.entrySet();
+//        for (Map.Entry<String, Integer> entry : listWordsNumber)
+//            System.out.printf("{Name: %s, occurence: %d}%n", entry.getKey(), entry.getValue());
+        List<WordOccurence> wordOccurences = new ArrayList<>();
+        for (String s : list) {
+            int indexOfExistence = checkExistence(wordOccurences, s);
+            if (indexOfExistence >= 0)
+                wordOccurences.get(indexOfExistence).setNum();
+        }
+    }
 
-        Set<Map.Entry<String, Integer>> listWordsNumber = wordsNumber.entrySet();
-        for (Map.Entry<String, Integer> entry : listWordsNumber)
-            System.out.printf("{Name: %s, occurence: %d}%n", entry.getKey(), entry.getValue());
+    public static int checkExistence(List<WordOccurence> wordOccurences, String s) {
+        for (int i = 0; i < wordOccurences.size(); i++)
+            if (s.equals(wordOccurences.get(i).getName()))
+                return i;
+        return -1;
     }
 }
